1568c239b73b72084331189fe078b14bf14cff65
Conor Barnes <conorbarnes93@gmail.com>
2021-10-07 21:48:51 -0300

    work with regular js as well. and files without extensions

--- src/main.rs
+++ src/main.rs
@@ -7,27 +7,40 @@ use regex::Regex;
 use std::io::prelude::*;
 use convert_case::{Case, Casing};
 
-/// Simple program to greet a person
+/// Create a .tsx file. 
 #[derive(StructOpt, Debug)]
 #[structopt(about = "CNRBRNS")]
-struct Cnr {
+struct TSX {
     names: Vec<String>
 }
 
 fn touch(name: &str) -> std::io::Result<()> {
-    let mut file = File::create(name)?;
-    let re = Regex::new(r"\w+\.tsx*").unwrap();
+    let re = Regex::new(r"\w+\.[jt]sx*").unwrap();
+
     if re.is_match(name) {
+        let mut file = File::create(name)?;
         let (pref, ext) = name.split_once(".").unwrap();
-        if ext == "tsx" {
-            let format_pref = pref.to_case(Case::Pascal);
-            let content = format!("export default function {}() {{\n\treturn (\n\n\t)\n}}", format_pref);
+        if ext == "tsx" || ext == "jsx" {
+            let format_name_cased = &pref.to_case(Case::Pascal);
+            let content = format!("export default function {}() {{\n\treturn (\n\n\t)\n}}", format_name_cased);
             file.write_all(content.as_bytes())?;
-        } else if ext == "ts" {
-            let format_pref = pref.to_case(Case::Camel);
-            let content = format!("export default function {}() {{\n\treturn (\n\n\t)\n}}", format_pref);
+        } else if ext == "ts" || ext == "js" {
+            let format_name_cased = &pref.to_case(Case::Camel);
+            let content = format!("export default function {}() {{\n\treturn (\n\n\t)\n}}", format_name_cased);
             file.write_all(content.as_bytes())?;
-        } 
+        }
+    } else {
+        
+        let mut file = if name.contains(".") {
+            File::create(format!("{}", name))?
+        } else {
+            File::create(format!("{}.tsx", name))?
+        };
+
+        let format_name = name.to_case(Case::Pascal);
+        let content = format!("export default function {}() {{\n\treturn (\n\n\t)\n}}", 
+            &format_name.split_once(".").unwrap_or((&format_name, "")).0);
+        file.write_all(content.as_bytes())?;
         
     }
     Ok(())
@@ -35,13 +48,19 @@ fn touch(name: &str) -> std::io::Result<()> {
 
 
 fn main() {
-    let args = Cnr::from_args();
-    println!("{:?}", args);
-    println!("{:?}", args.names);
+    let args = TSX::from_args();
 
+    if args.names.len() == 0 {
+        println!("Please include a file name.");
+        return
+    }
 
     for name in args.names.iter() {
         let res = touch(&name);
-    }
+        match res {
+            Err(e) => eprintln!("{}", e),
+            _ => println!(":)", ),
 
+        }
+    }
 }
