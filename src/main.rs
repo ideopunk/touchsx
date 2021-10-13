633f036c585d9d0a6c6b65aad4315dc3db40794d
Conor Barnes <conorbarnes93@gmail.com>
2021-10-13 20:06:28 -0300

    added test for odd subdirectory bug, remove regex lib

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,17 @@
-use std::fs::File;
+use std::fs;
 use std::io::prelude::*;
 use structopt::StructOpt;
-// use regex::Regex;
 use convert_case::{Case, Casing};
-#[cfg(test)]
-use std::path::Path;
 
 /// Create a Typescript file. 
 #[derive(StructOpt, Debug)]
-#[structopt(about = "Create a Javascript file")]
+#[structopt(about = "Create a Typescript file")]
 struct TSX {
     /// Name of file
     names: Vec<String>
 }
 
-fn touch(name: &str) -> std::io::Result<()> {
-
+pub fn touch(name: &str) -> std::io::Result<()> {
     // add a file extension if none exists
     let extended_name = if name.contains(".") {
         String::from(name)
@@ -26,10 +22,9 @@ fn touch(name: &str) -> std::io::Result<()> {
     let clone_name = extended_name.clone();
 
     // create the file
-    let mut file = File::create(extended_name)?;
+    let mut file = fs::File::create(extended_name)?;
 
     // snag the function name from the file name
-
     let (pref, ext) = clone_name.split_once(".").unwrap();
 
     let simple_name = if pref.contains("/") {
@@ -52,7 +47,7 @@ fn touch(name: &str) -> std::io::Result<()> {
     Ok(())
 }
 
-fn touches(names: Vec<String>) {
+pub fn touches(names: Vec<String>) {
     for name in names.iter() {
         let res = touch(&name);
         match res {
@@ -75,21 +70,35 @@ fn main() {
 }
 
 #[cfg(test)]
-pub fn touch_one_file_with_ext() {
-    let _ = touch("fake.tsx");
-    assert_eq!(true, Path::new("fake.tsx").exists())
-}
+mod tests {
+    use super::*;
+    use std::path::Path;
+    #[test]
+    fn touch_one_file_with_ext() {
+        let _ = touch("fake.tsx");
+        assert_eq!(true, Path::new("fake.tsx").exists())
+    }
 
-#[cfg(test)]
-pub fn touch_one_file_without_ext() {
-    let _ = touch("fake");
-    assert_eq!(true, Path::new("fake.tsx").exists())
-}
+    #[test]
+    fn touch_one_file_without_ext() {
+        let _ = touch("fake");
+        assert_eq!(true, Path::new("fake.tsx").exists())
+    }
 
-#[cfg(test)]
-pub fn touch_multiple_files() {
-    let _ = touches(vec![String::from("gom"), String::from("jabbar")]);
-    assert_eq!(true, Path::new("gom.tsx").exists());
-    assert_eq!(true, Path::new("jabbar.tsx").exists())
+    #[test]
+    fn touch_multiple_files() {
+        let _ = touches(vec![String::from("gom"), String::from("jabbar")]);
+        assert_eq!(true, Path::new("gom.tsx").exists());
+        assert_eq!(true, Path::new("jabbar.tsx").exists())
+    
+    }
 
+    #[test]
+    fn touch_file_in_subdirectory() {
+        let _ = touch("src/fake");
+        let contents = fs::read_to_string("src/fake.tsx").unwrap();
+        println!("{}", contents);
+        let proper_contents = contents.contains(" Fake");
+        assert_eq!(true, proper_contents)
+    }
 }
