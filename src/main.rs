0ae0ebc8f3d212c7d1def7775109f7fd4a7e2d70
Conor Barnes <conorbarnes93@gmail.com>
2022-02-08 20:32:25 -0400

    use pathbufs

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,64 @@
+use convert_case::{Case, Casing};
+use std::error::Error;
 use std::fs;
 use std::io::prelude::*;
+use std::path::{Path, PathBuf};
 use structopt::StructOpt;
-use convert_case::{Case, Casing};
 
-/// Create a Typescript file. 
+/// Create a Typescript file.
 #[derive(StructOpt, Debug)]
 #[structopt(about = "Create a Typescript file")]
 struct TSX {
     /// Name of file
-    names: Vec<String>
+    names: Vec<PathBuf>,
 }
 
-pub fn touch(name: &str) -> std::io::Result<()> {
-
+pub fn touch(prename: &PathBuf) -> std::io::Result<()> {
     // don't let the ref to current directory confuse the program.
-    let not_relative_name = if name.starts_with("./") {
-        let (_, rest) = name.split_at(2);
-        rest
-    } else {
-        name
-    };
-
+    // let not_relative_name = if name.starts_with("./") {
+    //     let (_, rest) = name..split_at(2);
+    //     rest
+    // } else {
+    //     name
+    // };
+    let mut name = prename.clone();
     // add a file extension if none exists
-    let extended_name = if not_relative_name.contains(".") {
-        String::from(not_relative_name)
-    } else {
-        format!("{}.tsx", not_relative_name)
+    let _ = match name.extension() {
+        None => name.set_extension("tsx"),
+        _ => true,
     };
 
-    let clone_name = extended_name.clone();
+    let clone_name = name.clone();
 
     // create the file
-    let mut file = fs::File::create(extended_name)?;
+    let mut file = fs::File::create(clone_name)?;
 
     // snag the function name from the file name
-    let (pref, ext) = clone_name.split_once(".").unwrap();
+    let stem = name.file_stem().unwrap();
 
-    let simple_name = if pref.contains("/") {
-        pref.rsplitn(2, "/").collect::<Vec<&str>>()[0]
-    } else {
-        pref
-    };
-
-
-    let format_name_cased =  if ext == "tsx" || ext == "jsx" {
-        simple_name.to_case(Case::Pascal)
-    } else  {
-        simple_name.to_case(Case::Camel)
-    }; 
+    let format_name_cased =
+        if name.extension().unwrap() == "tsx" || name.extension().unwrap() == "jsx" {
+            stem.to_str().unwrap().to_case(Case::Pascal)
+        } else {
+            stem.to_str().unwrap().to_case(Case::Camel)
+        };
 
     // write out the boilerplate
-    let content = format!("export default function {}() {{\n\treturn (\n\n\t)\n}}", format_name_cased);
+    let content = format!(
+        "export default function {}() {{\n\treturn (\n\n\t)\n}}",
+        format_name_cased
+    );
     file.write_all(content.as_bytes())?;
- 
+
     Ok(())
 }
 
-pub fn touches(names: Vec<String>) {
+pub fn touches(names: Vec<PathBuf>) {
     for name in names.iter() {
         let res = touch(&name);
         match res {
             Err(e) => eprintln!("{}", e),
             _ => (),
-
         }
     }
 }
@@ -72,7 +68,7 @@ fn main() {
 
     if args.names.len() == 0 {
         println!("Please include a file name.\nTry 'touchsx --help' for more information.");
-        return
+        return;
     }
 
     touches(args.names);
@@ -84,27 +80,26 @@ mod tests {
     use std::path::Path;
     #[test]
     fn touch_one_file_with_ext() {
-        let _ = touch("fake.tsx");
+        let _ = touch(&PathBuf::from("fake.tsx"));
         assert_eq!(true, Path::new("fake.tsx").exists())
     }
 
     #[test]
     fn touch_one_file_without_ext() {
-        let _ = touch("fake");
+        let _ = touch(&PathBuf::from("fake"));
         assert_eq!(true, Path::new("fake.tsx").exists())
     }
 
     #[test]
     fn touch_multiple_files() {
-        let _ = touches(vec![String::from("gom"), String::from("jabbar")]);
+        let _ = touches(vec![PathBuf::from("gom"), PathBuf::from("jabbar")]);
         assert_eq!(true, Path::new("gom.tsx").exists());
         assert_eq!(true, Path::new("jabbar.tsx").exists())
-    
     }
 
     #[test]
     fn touch_file_in_subdirectory() {
-        let _ = touch("src/fake");
+        let _ = touch(&PathBuf::from("src/fake"));
         let contents = fs::read_to_string("src/fake.tsx").unwrap();
         println!("{}", contents);
         let proper_contents = contents.contains(" Fake");
